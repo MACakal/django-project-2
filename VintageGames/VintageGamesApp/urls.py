d1e0b299b743ba3d6a4af9d0f43a81e9fc5751cf
MACakal <griffith7207@gmail.com>
2026-04-09 11:39:24 +0200

    added empty views so that the server runs

--- VintageGames/VintageGamesApp/urls.py
+++ VintageGames/VintageGamesApp/urls.py
@@ -1,24 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.homepage, name="home"),
-
     path('', include('django.contrib.auth.urls')),
-
-    path("register/", views.register, name="register"),
-
     path("games/", views.games, name="games"),
+    path("register/", views.register, name="register"),
     path("suggest_game/", views.suggest_game, name="suggest_game"),
     path("unapproved_games/", views.unapproved_games, name="unapproved_games"),
-
     path("new_sessions/", views.new_sessions, name="new_sessions"),
     path("my_sessions/", views.my_sessions, name="my_sessions"),
-
     path("edit_profile/", views.edit_profile, name="edit_profile"),
-
     path("newsfeed/", views.newsfeed, name="newsfeed"),
 ]
 
--- VintageGames/VintageGamesApp/views.py
+++ VintageGames/VintageGamesApp/views.py
@@ -1,32 +1,49 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .forms import GameForm, SessionForm, ProfileForm, Session
-from .models import Game
-from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
-from django.db.models import F
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
 def homepage(request):
     return render(request, "base/index.html")
 
-@login_required
-def games(request):
-    games = Game.objects.all()
-    context = {'games': games}
-    return render(request, 'base/games.html', context)
-
-def register(requests):
-    if requests.method == "POST":
-        form = UserCreationForm(requests.POST)
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(requests, user)
+            login(request, user)
             return redirect("home")
     else:
         form = UserCreationForm()
-    context = {"form":form}
-    return render(requests, "registration/register.html", context)
+    context = {"form": form}
+    return render(request, "registration/register.html", context)
+
+@login_required
+def games(request):
+    return render(request, "base/games.html")
+
+@login_required
+def suggest_game(request):
+    return render(request, "base/suggest_game.html")
+
+@staff_member_required
+def unapproved_games(request):
+    return render(request, "base/unapproved_games.html")
+
+@login_required
+def new_sessions(request):
+    return render(request, "base/new_sessions.html")
+
+@login_required
+def my_sessions(request):
+    return render(request, "base/my_sessions.html")
+
+@login_required
+def edit_profile(request):
+    return render(request, "base/edit_profile.html")
+
+@login_required
+def newsfeed(request):
+    return render(request, "base/newsfeed.html")
