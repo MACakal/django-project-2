e35a4994eaf96c55053fa180071a7f0d1442e233
MACakal <griffith7207@gmail.com>
2026-04-09 11:33:31 +0200

    urls and changed base to accomadate this project

--- VintageGames/VintageGamesApp/urls.py
+++ VintageGames/VintageGamesApp/urls.py
@@ -1,14 +1,26 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-from .models import Profile
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-
     path("", views.homepage, name="home"),
-    path('', include('django.contrib.auth.urls')),
-    path("games/", views.games, name="games"),
-    path("register/", views.register, name="register")
 
+    path('', include('django.contrib.auth.urls')),
+
+    path("register/", views.register, name="register"),
+
+    path("games/", views.games, name="games"),
+    path("suggest_game/", views.suggest_game, name="suggest_game"),
+    path("unapproved_games/", views.unapproved_games, name="unapproved_games"),
+
+    path("new_sessions/", views.new_sessions, name="new_sessions"),
+    path("my_sessions/", views.my_sessions, name="my_sessions"),
+
+    path("edit_profile/", views.edit_profile, name="edit_profile"),
+
+    path("newsfeed/", views.newsfeed, name="newsfeed"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
