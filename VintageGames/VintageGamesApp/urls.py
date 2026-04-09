f20f1d158e4e6b64a87696cbd995371dd6190d2b
MACakal <griffith7207@gmail.com>
2026-04-09 11:17:10 +0200

    added login

--- VintageGames/VintageGamesApp/urls.py
+++ VintageGames/VintageGamesApp/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 from . import views
+from .models import Profile
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
 
     path("", views.homepage, name="home"),
+    path('', include('django.contrib.auth.urls')),
+    path("games/", views.games, name="games"),
+    path("register/", views.register, name="register")
 
 ]
--- VintageGames/VintageGamesApp/views.py
+++ VintageGames/VintageGamesApp/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.shortcuts import redirect
 from .forms import GameForm, SessionForm, ProfileForm, Session
+from .models import Game
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
@@ -11,4 +11,21 @@ from django.db.models import F
 
 # Create your views here.
 def homepage(request):
-    return render(request,"index.html")
+    return render(request, "base/index.html")
+
+def games(request):
+    games = Game.objects.all()
+    context = {'games': games}
+    return render(request, 'base/games.html', context)
+
+def register(requests):
+    if requests.method == "POST":
+        form = UserCreationForm(requests.POST)
+        if form.is_valid():
+            user = form.save()
+            login(requests, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    context = {"form":form}
+    return render(requests, "registration/register.html", context)
