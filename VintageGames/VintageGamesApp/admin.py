228ffec48b5acb4f33cb376b60d9b5a4bb4f4847
Ahmad200ali <1116960@hr.nl>
2026-04-09 10:14:44 +0200

    making models for the project and fixing temapltes and change profielmodel

--- VintageGames/VintageGamesApp/admin.py
+++ VintageGames/VintageGamesApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Profile,Games,Session
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Games)
+admin.site.register(Session)
